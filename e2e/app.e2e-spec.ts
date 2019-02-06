f64484d67945e73a8d99bfddfd91b2ff8f68f459
Sinu Joseph Madathiparambil <sinuj@tradeix.com>
2019-02-06 23:11:54 +0000

    E2E Tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,7 +7,29 @@ describe('angular-weather App', () => {
     page = new AppPage();
   });
 
-  it('e2e are running empty, please implement', () => {
-    expect(page);
+  it('should the correct application heading', () => {
+    // Act
+    page.navigateTo();
+    // Assert
+    expect(page.getParagraphText()).toEqual('AgileSphere coding test - The Weather App');
+  });
+
+  it('should search weather in Bedford city', () => {
+    // Act
+    page.getCitySearchBox().sendKeys('Bedford');
+    page.getCitySearchButton().click().then(() => {
+      // Assert
+      expect(page.getListOfTableRows().count()).toEqual(1);
+      expect(page.getListOfCityNames().get(0).getText()).toEqual('Bedford');
+    });
+  });
+
+  it('should have table with nine columns', () => {
+    // Act
+    page.getCitySearchBox().sendKeys('Bedford');
+    page.getCitySearchButton().click().then(() => {
+      // Assert
+      expect(page.getListOfTableColumns().count()).toEqual(9);
+    });
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,27 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root a')).getText();
+  }
+
+  getListOfCityNames() {
+    return element.all(by.css('app-results tbody tr td'));
+  }
+
+  getCitySearchBox() {
+    return element(by.css('app-search input#city'));
+  }
+
+
+  getListOfTableRows() {
+    return element.all(by.css('app-results tbody tr'));
+  }
+
+  getCitySearchButton() {
+    return element(by.css('app-search button[type="submit"]'));
+  }
+
+  getListOfTableColumns() {
+    return element.all(by.css('app-results tbody tr:first-child td'));
   }
 }
